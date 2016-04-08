d0f77fa199b3cfbe31b2fa6d845babc3850d3e9f
Rik Heijdens <rik@jwplayer.com>
2016-04-08 10:30:24 +0200

    Optimized the ListView speed with a placeholder ImageView

--- listview-fullscreen/app/src/main/java/com/jwplayer/demo/listview/PlayerAdapter.java
+++ listview-fullscreen/app/src/main/java/com/jwplayer/demo/listview/PlayerAdapter.java
@@ -6,10 +6,13 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
+import android.widget.FrameLayout;
+import android.widget.ImageView;
 import android.widget.ListView;
 import android.widget.TextView;
 
 import com.longtailvideo.jwplayer.JWPlayerView;
+import com.longtailvideo.jwplayer.configuration.PlayerConfig;
 import com.longtailvideo.jwplayer.configuration.Skin;
 import com.longtailvideo.jwplayer.fullscreen.FullscreenHandler;
 import com.longtailvideo.jwplayer.media.playlists.PlaylistItem;
@@ -21,21 +24,18 @@ public class PlayerAdapter extends BaseAdapter {
     private static final String SKIN_URL = "file:///android_asset/listview-skin.css";
 
     private Activity mActivity;
-    private ListView mListView;
     private PlaylistItem[] mPlaylist;
     private LayoutInflater mLayoutInflater;
     private View mDecorView;
     private ViewGroup mRootView;
     private HashSet<JWPlayerView> mPlayerViews = new HashSet<>();
-    private ViewGroup.LayoutParams mLayoutParams;
-    private JWPlayerView mFullscreenedPlayer;
+    private JWPlayerView mFullscreenPlayer;
 
     public PlayerAdapter(Activity activity, ListView listView, PlaylistItem[] listItems) {
         mActivity = activity;
-        mListView = listView;
         mPlaylist = listItems;
         mLayoutInflater = LayoutInflater.from(activity);
-        mListView.setAdapter(this);
+        listView.setAdapter(this);
         mDecorView = activity.getWindow().getDecorView();
         mRootView = (ViewGroup) activity.findViewById(android.R.id.content);
     }
@@ -59,131 +59,65 @@ public class PlayerAdapter extends BaseAdapter {
     public View getView(int position, View convertView, ViewGroup parent) {
         if (isFullscreen()) {
             // Don't bother doing anything if list items won't be visible anyway.
-            // This prevents a call to load() on the JWPlayerView for every list item in the
-            // list that is not visible, which greatly improves performance.
             return convertView;
         }
 
         if (convertView == null) {
+            // We need to instantiate a new view.
             convertView = mLayoutInflater.inflate(R.layout.list_item, null);
         }
 
-        final ViewGroup listItem = (ViewGroup) convertView;
+        // Get references to the UI elements
         TextView title = (TextView) convertView.findViewById(R.id.title);
         TextView description = (TextView) convertView.findViewById(R.id.description);
+        final FrameLayout playerContainer = (FrameLayout) convertView.findViewById(R.id.player_container);
+        final ImageView placeholder = (ImageView) playerContainer.findViewById(R.id.player_placeholder);
 
-        final JWPlayerView playerView = (JWPlayerView) convertView.findViewById(R.id.player);
-        if (playerView == null) {
-            // If the playerView is null, then we are in fullscreen mode.
-            // In that case it doesn't really matter what we return since it won't be visible anyway.
-            return convertView;
-        }
-
-        mPlayerViews.add(playerView);
-        PlaylistItem playlistItem = (PlaylistItem) getItem(position);
+        // Update the UI
+        final PlaylistItem playlistItem = (PlaylistItem) getItem(position);
         title.setText(playlistItem.getTitle());
         description.setText(playlistItem.getDescription());
-        playerView.setSkin(SKIN_URL);
-        playerView.setFullscreenHandler(new FullscreenHandler() {
-            @Override
-            public void onFullscreenRequested() {
-                // Hide system ui
-                hideSystemUI();
 
-                // Enter landscape mode for fullscreen videos
-                mActivity.setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
-
-                // Keep a copy of the layout params. We'll use this to restore the player when returning
-                // from fullscreen.
-                mLayoutParams = playerView.getLayoutParams();
-
-                // Destroy the player's rendering surface, we need to do this to prevent Android's
-                // MediaDecoders from crashing.
-                playerView.destroySurface();
-
-                // Remove the JWPlayerView from the list item.
-                listItem.removeView(playerView);
-
-                // Use the JW7 skin in fullscreen.
-                playerView.setSkin(Skin.SEVEN);
-
-                // Initialize a new rendering surface.
-                playerView.initializeSurface();
-
-                // Add the JWPlayerView to the RootView as soon as the UI thread is ready.
-                mRootView.post(new Runnable() {
-                    @Override
-                    public void run() {
-                        mRootView.addView(playerView, new ViewGroup.LayoutParams(
-                                ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.MATCH_PARENT));
-                        mFullscreenedPlayer = playerView;
-                    }
-                });
+        // Check if there is already a JWPlayerView present in the convertView.
+        // If there is, then we want to recycle it, because player instantiation is quite expensive.
+        JWPlayerView player = null;
+        for (int i = 0; i < playerContainer.getChildCount(); i++) {
+            if (playerContainer.getChildAt(i) instanceof JWPlayerView) {
+                // Recycle the JWPlayerView
+                player = (JWPlayerView) playerContainer.getChildAt(i);
+                if (player.getPlaylistItem(player.getPlaylistIndex()) != playlistItem) {
+                    // Make sure the player has been released.
+                    player.stop();
+                    // Hide the player, it will be shown again when a user interacts with the ImageView.
+                    player.setVisibility(View.GONE);
+                }
             }
+        }
 
+        final JWPlayerView finalPlayer = player;
+        placeholder.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onFullscreenExitRequested() {
-                // Show the system ui
-                showSystemUI();
-
-                // Enter portrait mode
-                mActivity.setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
-
-                // Destroy the surface that is used for video output, we need to do this before
-                // we can detach the JWPlayerView from a ViewGroup.
-                playerView.destroySurface();
-
-                // Remove the player view from the root ViewGroup.
-                mRootView.removeView(playerView);
-
-                // After we've detached the JWPlayerView we can safely reinitialize the surface.
-                playerView.initializeSurface();
-
-                // Restore the listview-skin
-                playerView.setSkin(SKIN_URL);
-
-                // As soon as the UI thread has finished processing the current message queue it
-                // should add the JWPlayerView back to the list item.
-                listItem.post(new Runnable() {
-                    @Override
-                    public void run() {
-                        listItem.addView(playerView, mLayoutParams);
-                        mFullscreenedPlayer = null;
-                    }
-                });
-            }
-
-            @Override
-            public void onResume() {
-                // Do nothing, we're not listening for device rotation changes.
-            }
-
-            @Override
-            public void onPause() {
-
-            }
-
-            @Override
-            public void onDestroy() {
-
-            }
-
-            @Override
-            public void onAllowRotationChanged(boolean allowRotation) {
-
-            }
-
-            @Override
-            public void updateLayoutParams(ViewGroup.LayoutParams layoutParams) {
-
-            }
-
-            @Override
-            public void setUseFullscreenLayoutFlags(boolean useFlags) {
-
+            public void onClick(View v) {
+                // As soon as a user interacts with the ImageView, swap it out for the JW Player.
+                // We're using ImageView's in order to optimize ListView performance.
+                JWPlayerView playerView;
+                if (finalPlayer == null) {
+                    playerView = new JWPlayerView(mActivity, new PlayerConfig.Builder().build());
+                    playerView.setFullscreenHandler(new ListViewFullscreenHandler(
+                            playerContainer, playerView));
+                    playerView.setSkin(SKIN_URL);
+                    mPlayerViews.add(playerView);
+                    playerContainer.addView(playerView);
+                } else {
+                    // Recycle an existing player.
+                    playerView = finalPlayer;
+                    playerView.setVisibility(View.VISIBLE);
+                }
+                playerView.load(playlistItem);
+                playerView.play(true);
             }
         });
-        playerView.load(playlistItem);
+
         return convertView;
     }
 
@@ -214,15 +148,15 @@ public class PlayerAdapter extends BaseAdapter {
      * @return true if a player is in fullscreen mode.
      */
     public boolean isFullscreen() {
-        return mFullscreenedPlayer != null;
+        return mFullscreenPlayer != null;
     }
 
     /**
      * Handles the back button pressed event.
      */
     public void onBackPressed() {
-        if (mFullscreenedPlayer != null) {
-            mFullscreenedPlayer.setFullscreen(false, false);
+        if (mFullscreenPlayer != null) {
+            mFullscreenPlayer.setFullscreen(false, false);
         }
     }
 
@@ -247,5 +181,115 @@ public class PlayerAdapter extends BaseAdapter {
                         | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION // hide nav bar
                         | View.SYSTEM_UI_FLAG_FULLSCREEN // hide status bar
                         | View.SYSTEM_UI_FLAG_IMMERSIVE);
+    }
+
+    /**
+     * A FullscreenHandler for JWPlayerView's in a ListView.
+     */
+    private class ListViewFullscreenHandler implements FullscreenHandler {
+
+        private ViewGroup mPlayerContainer;
+        private JWPlayerView mPlayerView;
+
+        public ListViewFullscreenHandler(ViewGroup playerContainer, JWPlayerView playerView) {
+            mPlayerContainer = playerContainer;
+            mPlayerView = playerView;
+        }
+
+        @Override
+        public void onFullscreenRequested() {
+            // Hide system ui
+            hideSystemUI();
+
+            // Enter landscape mode for fullscreen videos
+            mActivity.setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
+
+            // Destroy the player's rendering surface, we need to do this to prevent Android's
+            // MediaDecoders from crashing.
+            mPlayerView.destroySurface();
+
+            // Remove the JWPlayerView from the list item.
+            mPlayerContainer.removeView(mPlayerView);
+
+            // Use the JW7 skin in fullscreen.
+            mPlayerView.setSkin(Skin.SEVEN);
+
+            // Initialize a new rendering surface.
+            mPlayerView.initializeSurface();
+
+            // Add the JWPlayerView to the RootView as soon as the UI thread is ready.
+            mRootView.post(new Runnable() {
+                @Override
+                public void run() {
+                    mRootView.addView(mPlayerView, new ViewGroup.LayoutParams(
+                            ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.MATCH_PARENT));
+                    mFullscreenPlayer = mPlayerView;
+                }
+            });
+        }
+
+        @Override
+        public void onFullscreenExitRequested() {
+            // Show the system ui
+            showSystemUI();
+
+            // Enter portrait mode
+            mActivity.setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
+
+            // Destroy the surface that is used for video output, we need to do this before
+            // we can detach the JWPlayerView from a ViewGroup.
+            mPlayerView.destroySurface();
+
+            // Remove the player view from the root ViewGroup.
+            mRootView.removeView(mPlayerView);
+
+            // After we've detached the JWPlayerView we can safely reinitialize the surface.
+            mPlayerView.initializeSurface();
+
+            // Restore the listview-skin
+            mPlayerView.setSkin(SKIN_URL);
+
+            // As soon as the UI thread has finished processing the current message queue it
+            // should add the JWPlayerView back to the list item.
+            mPlayerContainer.post(new Runnable() {
+                @Override
+                public void run() {
+                    mPlayerContainer.addView(mPlayerView, new ViewGroup.LayoutParams(
+                            ViewGroup.LayoutParams.MATCH_PARENT,
+                            ViewGroup.LayoutParams.MATCH_PARENT));
+                    mFullscreenPlayer = null;
+                }
+            });
+        }
+
+        @Override
+        public void onResume() {
+            // Do nothing, we're not listening for device rotation changes.
+        }
+
+        @Override
+        public void onPause() {
+
+        }
+
+        @Override
+        public void onDestroy() {
+
+        }
+
+        @Override
+        public void onAllowRotationChanged(boolean allowRotation) {
+
+        }
+
+        @Override
+        public void updateLayoutParams(ViewGroup.LayoutParams layoutParams) {
+
+        }
+
+        @Override
+        public void setUseFullscreenLayoutFlags(boolean useFlags) {
+
+        }
     }
 }
