8a25d8dc526d9c40000d5cbf1d397a1bceb7a8e1
Jordan Joyner <jordan@brooklynpacket.com>
2019-07-29 16:47:20 -0400

    [SDK-4110] Add a demo app for demoing native controls (#19)
    * Add a demo app for demoing native controls
    
    * Handle back button pressed blowing up the app in full screen
    
    * Addressing some feedback from Paul
    
    Adding a replay icon
    Swap out a padding function for string.format
    Disable most UI when displaying a stream instead of a video
    
    * Incoporated feedback from paul
    
    Hamburger menu added to swap between stream and video playback
    
    * Add an archived DVR hockey stream and to demo scrubbale streams

---
+++ DemoNativeControls/app/src/main/java/com/jwplayer/demo/nativecontrols/JWPlayerNativeControls.java
@@ -0,0 +1,296 @@
+package com.jwplayer.demo.nativecontrols;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.RelativeLayout;
+import android.widget.SeekBar;
+import android.widget.TextView;
+
+import com.longtailvideo.jwplayer.JWPlayerView;
+import com.longtailvideo.jwplayer.core.PlayerState;
+import com.longtailvideo.jwplayer.events.CompleteEvent;
+import com.longtailvideo.jwplayer.events.PauseEvent;
+import com.longtailvideo.jwplayer.events.PlayEvent;
+import com.longtailvideo.jwplayer.events.TimeEvent;
+import com.longtailvideo.jwplayer.events.listeners.VideoPlayerEvents;
+
+import java.util.ArrayList;
+
+public class JWPlayerNativeControls extends RelativeLayout
+        implements View.OnClickListener,
+        VideoPlayerEvents.OnPlayListener,
+        VideoPlayerEvents.OnPauseListener,
+        VideoPlayerEvents.OnCompleteListener{
+
+    public interface OnControlsInteraction {
+        void OnControlsInteractedWith(boolean triggeredPlayback);
+        void onProlongedInteractionBegan();
+        void onProlongedInteractionEnded();
+    }
+    ArrayList<OnControlsInteraction> mControlChangeListeners = new ArrayList<>();
+
+    JWPlayerView    mPlayerView;
+    SeekBar         mSeekBar;
+    TextView        mSeekBarText;
+    JWProgressBar   mVideoProgressBar;
+    Button          mMuteButton;
+    ImageButton     mPlayPauseButton;
+    ImageButton     mFullScreenButton;
+    ImageButton     mFastForwardButton;
+    ImageButton     mRewindButton;
+
+    public JWPlayerNativeControls(Context context) {
+        this(context, null, 0);
+    }
+
+    public JWPlayerNativeControls(Context context, AttributeSet attrs) {
+        this(context, attrs, 0);
+    }
+
+    public JWPlayerNativeControls(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+        init(context);
+    }
+
+    private void init(Context context) {
+        View v = inflate(getContext(), R.layout.native_controls, this);
+
+        mPlayPauseButton = v.findViewById(R.id.play_and_pause_button);
+        v.findViewById(R.id.stop_button).setOnClickListener(this);
+        mFastForwardButton = v.findViewById(R.id.fast_forward_button);
+        mRewindButton = v.findViewById(R.id.rewind_button);
+        mFullScreenButton = v.findViewById(R.id.fullscreen_button);
+        mSeekBar = v.findViewById(R.id.seek_bar);
+        mSeekBarText = v.findViewById(R.id.seekbar_textview);
+        mMuteButton = v.findViewById(R.id.mute_button);
+
+        mPlayPauseButton.setOnClickListener(this);
+        mFastForwardButton.setOnClickListener(this);
+        mRewindButton.setOnClickListener(this);
+        mFullScreenButton.setOnClickListener(this);
+        mMuteButton.setOnClickListener(this);
+
+        mVideoProgressBar = new JWProgressBar();
+        mSeekBar.setOnSeekBarChangeListener(mVideoProgressBar);
+        resetVideoPlaybackUI();
+    }
+
+    public void resetVideoPlaybackUI(){
+        mPlayPauseButton.setImageResource(R.drawable.exo_controls_play);
+        mSeekBar.setProgress(0);
+        mSeekBarText.setText("00:00 / 00:00");
+    }
+
+    public void addControlsInteractionListener(OnControlsInteraction ocl){
+        mControlChangeListeners.add(ocl);
+    }
+
+    public boolean removeControlsInteractionListener(OnControlsInteraction ocl){
+        return mControlChangeListeners.remove(ocl);
+    }
+
+    public void setJWView(JWPlayerView playerView) {
+        mPlayerView = playerView;
+        //Sign us up for player related events so we can appopriately update UI
+        //Play and pause will be used to toggle the play/pause button and it's functionality
+        mPlayerView.addOnPlayListener(this);
+        mPlayerView.addOnPauseListener(this);
+        //We'll use time event to update the seek bar time
+        mPlayerView.addOnTimeListener(mVideoProgressBar);
+        //Listen to the complete event to update the play pause UI
+        mPlayerView.addOnCompleteListener(this);
+    }
+
+    public void unSubscribeFromJWEvents(){
+        mPlayerView.removeOnPlayListener(this);
+        mPlayerView.removeOnPauseListener(this);
+        mPlayerView.removeOnTimeListener(mVideoProgressBar);
+        mPlayerView.removeOnCompleteListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        boolean stoppedPlayback = false;
+        switch (v.getId()) {
+            case R.id.play_and_pause_button:
+                //If we're playing stop us, if we're not playing play us
+                if(mPlayerView.getState() == PlayerState.PLAYING){
+                    mPlayerView.pause();
+                    stoppedPlayback = true;
+                } else if(mPlayerView.getState() != PlayerState.PLAYING){
+                    mPlayerView.play();
+                }
+                break;
+            case R.id.stop_button:
+                //Cancel playback ASAP and reset the UI back to the pre video starts
+                stoppedPlayback = true;
+                mPlayerView.stop();
+                resetVideoPlaybackUI();
+                break;
+            case R.id.fullscreen_button:
+                if(mPlayerView.getFullscreen()){
+                    mPlayerView.setFullscreen(false,true);
+                } else {
+                    mPlayerView.setFullscreen(true,true);
+                }
+                break;
+            case R.id.fast_forward_button:
+                mPlayerView.seek(mPlayerView.getPosition() + 10);
+                break;
+            case R.id.rewind_button:
+                double newPosition = mPlayerView.getPosition() - 10;
+                //We clamp to 0 because JWPlayer would use negative numbers to jump to <end of video> - seekPos
+                //which would make -10, 10 seconds from the end of the video
+                if(newPosition < 0){
+                    newPosition = 0;
+                }
+                mPlayerView.seek(newPosition);
+                break;
+            case R.id.mute_button:
+                if(mPlayerView.getState() == PlayerState.PLAYING){
+                    boolean muted = mPlayerView.getMute();
+                    if(muted){ //we're gonna unmute it
+                        mMuteButton.setText("Mute");
+                    } else { //We're gonna mute it
+                        mMuteButton.setText("UnMute");
+                    }
+                    mPlayerView.setMute(!muted);
+                }
+                break;
+        }
+
+        //Notify all our listeners that one of the mNativePlayerControls has been interacted with
+        for (OnControlsInteraction ocl: mControlChangeListeners) {
+            ocl.OnControlsInteractedWith(stoppedPlayback);
+        }
+    }
+
+    @Override
+    public void onPlay(PlayEvent playEvent) {
+        mPlayPauseButton.setImageResource(R.drawable.exo_controls_pause);
+    }
+
+    @Override
+    public void onPause(PauseEvent pauseEvent) {
+        mPlayPauseButton.setImageResource(R.drawable.exo_controls_play);
+    }
+
+    @Override
+    public void onComplete(CompleteEvent completeEvent) {
+        mPlayPauseButton.setImageResource(R.mipmap.ic_replay);
+    }
+
+    private class JWProgressBar implements VideoPlayerEvents.OnTimeListener,
+            SeekBar.OnSeekBarChangeListener {
+        //Ignore time events when the user is using the scrub to navigate the video otherwise the scrubber will jump erratically
+        private boolean ignoreTimeEvents = false;
+
+        @Override
+        public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+            //Convert from progress to a time stamp to display to the user
+            double videoDuration = mPlayerView.getDuration();
+            String seekbarMessage = "";
+            if(videoDuration > 0){
+                double currentTime = progress;
+                int currentMinutes = (int)currentTime / 60;
+                int currentSeconds = (int)currentTime % 60;
+                int finalMinutes = (int)videoDuration / 60;
+                int finalSeconds = (int)videoDuration % 60;
+
+
+                seekbarMessage = String.format("%02d", currentMinutes) +
+                                ":" +
+                                String.format("%02d", currentSeconds) +
+                                " / " +
+                                String.format("%02d", finalMinutes) +
+                                ":" +
+                                String.format("%02d", finalSeconds);
+            } else { // if videoDuration is < 0 we have a live stream so disable the scrubber
+                //seekbarMessage = "Streaming";
+                double currentTime = Math.abs(videoDuration + progress);
+                if(progress > 0 && videoDuration == 0){
+                    seekbarMessage = "LIVE";
+                } else {
+                    int currentMinutes = (int)currentTime / 60;
+                    int currentSeconds = (int)currentTime % 60;
+
+                    if(videoDuration < 0){
+                        seekbarMessage += "-";
+                    }
+                    //Manually add the negative sign so this plays nice with -00:02s
+                    seekbarMessage +=
+                            String.format("%02d", currentMinutes) +
+                                    ":" +
+                                    String.format("%02d", currentSeconds);
+                }
+            }
+            mSeekBarText.setText(seekbarMessage);
+            //If the user moved the scrubber jump to that timestamp in the video
+            //Convert that to seconds into the video and seek that far
+            if(fromUser){
+                if(videoDuration < 0){
+                    //To get the bar to look right, if we jump to live we set the seek to -1 instead of 0
+                    double seekValue = videoDuration + progress;
+                    if(seekValue == 0){
+                        seekValue = -1;
+                    }
+                    mPlayerView.seek(seekValue);
+                } else {
+                    mPlayerView.seek(progress);
+                }
+            }
+        }
+
+        @Override
+        public void onStartTrackingTouch(SeekBar seekBar) {
+            ignoreTimeEvents = true;
+            for (OnControlsInteraction ocl: mControlChangeListeners) {
+                ocl.onProlongedInteractionBegan();
+            }
+        }
+
+        @Override
+        public void onStopTrackingTouch(SeekBar seekBar) {
+            ignoreTimeEvents = false;
+            for (OnControlsInteraction ocl: mControlChangeListeners) {
+                ocl.onProlongedInteractionEnded();
+            }
+        }
+
+        @Override
+        public void onTime(TimeEvent timeEvent) {
+            if(ignoreTimeEvents){
+                return;
+            }
+            double position = timeEvent.getPosition();
+            double duration = timeEvent.getDuration();
+            //Time events will have a negative position if we're streaming
+            //and if we're streaming we should disable several UI elements like rewind, seek forward, and scrubber
+            int UIVisibility = VISIBLE;
+            if(timeEvent.getDuration() < 0){
+                UIVisibility = INVISIBLE;
+            }
+            mRewindButton.setVisibility(UIVisibility);
+            mFastForwardButton.setVisibility(UIVisibility);
+            mSeekBar.setVisibility(VISIBLE);
+
+            //if theres a negative duration then it's a stream
+            //if theres a positive position then it's a live stream you can't scrub through
+            //if theres a negative position it's a stream you CAN scrub through
+            int absDuration = (int)Math.abs(duration);
+            mSeekBar.setMax(absDuration);
+            if(duration < 0){
+                if(position < 0){
+                    position = absDuration - (int)Math.abs(position);
+                } else {
+                    mSeekBarText.setText("LIVE");
+                }
+            }
+            //Whenever we get an time update from the player move the seek bar forward in lock step
+            mSeekBar.setProgress((int)position);
+        }
+    }
+}
---
+++ DemoNativeControls/app/src/main/java/com/jwplayer/demo/nativecontrols/MainActivity.java
@@ -0,0 +1,234 @@
+package com.jwplayer.demo.nativecontrols;
+
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.RelativeLayout;
+
+import com.longtailvideo.jwplayer.JWPlayerView;
+import com.longtailvideo.jwplayer.configuration.PlayerConfig;
+import com.longtailvideo.jwplayer.events.ReadyEvent;
+import com.longtailvideo.jwplayer.events.listeners.VideoPlayerEvents;
+import com.longtailvideo.jwplayer.media.playlists.PlaylistItem;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity
+        implements View.OnClickListener,
+        VideoPlayerEvents.OnReadyListener,
+        JWPlayerNativeControls.OnControlsInteraction {
+    JWPlayerView mPlayerView;
+    JWPlayerNativeControls mNativePlayerControls;
+    RelativeLayout mTouchInterceptorView;
+
+    final Handler mHideControlsHandler = new Handler();
+    final long mDelayBeforeControlsAreHidden = 3000;
+    final String videoFileName = "http://samplescdn.origin.mediaservices.windows.net/e0e820ec-f6a2-4ea2-afe3-1eed4e06ab2c/AzureMediaServices_Overview.ism/manifest(format=m3u8-aapl-v3)";
+    final String streamFileName = "http://playertest.longtailvideo.com/adaptive/wowzaid3/playlist.m3u8";
+    final String scrubbaleStreamFileName = "http://playertest.longtailvideo.com/hls/hockey/new_master.m3u8";
+
+    //The runnable that will be responsible for hiding mNativePlayerControls
+    //This will trigger if the video is playing and the user doesn't interact with the screen
+    Runnable mHideControlsRunnable = new Runnable() {
+        @Override
+        public void run() {
+            //Hide our native controls
+            mNativePlayerControls.setVisibility(View.INVISIBLE);
+            enableTouchInterceptor();
+        }
+    };
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        mPlayerView = findViewById(R.id.jwplayer);
+
+        //Subscribe to any events we're interested in
+        mPlayerView.addOnReadyListener(this);
+
+        mNativePlayerControls = findViewById(R.id.player_control_view);
+        //Initialize our mNativePlayerControls
+        mNativePlayerControls.setJWView(mPlayerView);
+        //Subscribe to the mNativePlayerControls interaction events being emitted
+        //by the mNativePlayerControls to automatically fade the mNativePlayerControls out during playback
+        mNativePlayerControls.addControlsInteractionListener(this);
+
+
+
+        //You can demo stream functionality by using the other file url
+        String chosenFile = videoFileName;
+
+
+        //Setup our player with a demo playlist item so it plays a video
+        PlaylistItem playlistItem = new PlaylistItem.Builder()
+                .file(chosenFile)
+                .title(getString(R.string.demo_video_title))
+                .description("A video player testing video.")
+                .build();
+        List<PlaylistItem> playlist = new ArrayList<>();
+        playlist.add(playlistItem);
+        PlayerConfig config = new PlayerConfig.Builder().playlist(playlist).build();
+        mPlayerView.setup(config);
+
+        //Disable the player's controls so we don't have conflict with our native ones
+        mPlayerView.setControls(false);
+
+        //We're going to go full native
+        //So use a big view to intercept and manage clicks to get around some built in webplayer click functionality
+        mTouchInterceptorView = findViewById(R.id.touch_interceptor_relative_view);
+        mTouchInterceptorView.setOnClickListener(this);
+        mTouchInterceptorView.setVisibility(View.INVISIBLE);
+
+        //Hide the controls initially
+        //If we encounter a load error publicly display that
+        //Bring back the controls in the onReady
+        mNativePlayerControls.setVisibility(View.INVISIBLE);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        mPlayerView.onResume();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        mPlayerView.onPause();
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        mPlayerView.onStop();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        mPlayerView.onDestroy();
+        mNativePlayerControls.unSubscribeFromJWEvents();
+        mNativePlayerControls.removeControlsInteractionListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        //If we click on the touch interceptor and it's invisible
+        //make it visible so we can see the native controls again
+        //And set a delayed event to hide the native controls again if the user does nothing
+        if(v.getId() == R.id.touch_interceptor_relative_view){
+            int currentVisibility = mNativePlayerControls.getVisibility();
+            if(currentVisibility != View.VISIBLE){
+                mNativePlayerControls.setVisibility(View.VISIBLE);
+                mTouchInterceptorView.setVisibility(View.INVISIBLE);
+                startFadeControlsTimer();
+            }
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        super.onCreateOptionsMenu(menu);
+        getMenuInflater().inflate(R.menu.main_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        String chosenFile = "";
+        String video_title = getString(R.string.demo_video_title);
+        switch (item.getItemId()) {
+            case R.id.menu_swap_to_live_stream: {
+                chosenFile = streamFileName;
+                video_title = getString(R.string.demo_live_stream_title);
+                break;
+            }
+            case R.id.menu_swap_to_scrubbale_stream: {
+                chosenFile = scrubbaleStreamFileName;
+                video_title = getString(R.string.demo_archived_stream_title);
+                break;
+            }
+            case R.id.menu_swap_to_video: {
+                chosenFile = videoFileName;
+                video_title = getString(R.string.demo_video_title);
+                break;
+            }
+        }
+        //Setup our player with a demo playlist item so it plays a video
+        PlaylistItem playlistItem = new PlaylistItem.Builder()
+                .file(chosenFile)
+                .title(video_title)
+                .description("Demonstration of playback")
+                .build();
+        List<PlaylistItem> playlist = new ArrayList<>();
+        playlist.add(playlistItem);
+        PlayerConfig config = new PlayerConfig.Builder().playlist(playlist).build();
+        mPlayerView.stop();
+        mNativePlayerControls.resetVideoPlaybackUI();
+
+        //Cancel Timer to hide mNativePlayerControls altogether
+        mHideControlsHandler.removeCallbacks(mHideControlsRunnable);
+        mTouchInterceptorView.setClickable(false);
+        mTouchInterceptorView.setVisibility(View.INVISIBLE);
+        mPlayerView.setup(config);
+        mPlayerView.setControls(false);
+        return true;
+    }
+    //Controls Interaction Listener Events
+
+    //When mNativePlayerControls are interacted with we want to handle them
+    //automatically fading them out after a couple seconds for a smoother view experience
+    @Override
+    public void OnControlsInteractedWith(boolean stoppedPlayback) {
+        //If the mNativePlayerControls were interacted with reset the timer
+        mHideControlsHandler.removeCallbacks(mHideControlsRunnable);
+
+        if(!stoppedPlayback){
+            startFadeControlsTimer();
+        }
+    }
+
+    //When a prolonged interaction begins just cancel the fading until the interaction completes
+    @Override
+    public void onProlongedInteractionBegan() {
+        //Cancel Timer to hide mNativePlayerControls altogether
+        mHideControlsHandler.removeCallbacks(mHideControlsRunnable);
+    }
+
+    //Only resume the callback to fade out the mNativePlayerControls if at the end of
+    //the prolonged interaction the video is actually playing
+    @Override
+    public void onProlongedInteractionEnded() {
+        startFadeControlsTimer();
+    }
+
+    @Override
+    public void onReady(ReadyEvent readyEvent) {
+        //we're ready, the license key was accepted, let's bring up the controls
+        mNativePlayerControls.setVisibility(View.VISIBLE);
+    }
+
+    private void startFadeControlsTimer(){
+        mHideControlsHandler.postDelayed(mHideControlsRunnable,mDelayBeforeControlsAreHidden);
+    }
+
+    private void enableTouchInterceptor(){
+        mTouchInterceptorView.setClickable(true);
+        mTouchInterceptorView.setVisibility(View.VISIBLE);
+        mTouchInterceptorView.bringToFront();
+    }
+
+    @Override
+    public void onBackPressed() {
+        if(mPlayerView.getFullscreen()){
+            mPlayerView.setFullscreen(false,true);
+        } else {
+            super.onBackPressed();
+        }
+    }
+}
