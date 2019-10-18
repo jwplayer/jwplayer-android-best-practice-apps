f76f3de9952f4b3fb4c07b5a9de7b0715dff90f4
gmsyrimis <gmsyrimis@gmail.com>
2019-10-18 16:52:52 -0400

    optimized the view tree, improved implementation by extending JWPlayerView

--- movable-player/app/src/main/java/com/jwplayer/demo/movableplayer/MainActivity.java
+++ movable-player/app/src/main/java/com/jwplayer/demo/movableplayer/MainActivity.java
@@ -3,51 +3,22 @@ package com.jwplayer.demo.movableplayer;
 import android.content.res.Configuration;
 import android.os.Bundle;
 import android.util.DisplayMetrics;
-import android.util.Log;
 import android.view.KeyEvent;
-import android.view.MotionEvent;
-import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Button;
-import android.widget.FrameLayout;
 import android.widget.RelativeLayout;
 
-import com.longtailvideo.jwplayer.JWPlayerView;
 import com.longtailvideo.jwplayer.configuration.PlayerConfig;
-import com.longtailvideo.jwplayer.core.PlayerState;
-import com.longtailvideo.jwplayer.events.CompleteEvent;
 import com.longtailvideo.jwplayer.events.FullscreenEvent;
-import com.longtailvideo.jwplayer.events.PauseEvent;
-import com.longtailvideo.jwplayer.events.PlayEvent;
 import com.longtailvideo.jwplayer.events.listeners.VideoPlayerEvents;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.coordinatorlayout.widget.CoordinatorLayout;
 
 
 public class MainActivity extends AppCompatActivity implements
-													VideoPlayerEvents.OnFullscreenListener,
-													VideoPlayerEvents.OnPlayListener,
-													VideoPlayerEvents.OnPauseListener,
-													VideoPlayerEvents.OnCompleteListener {
-
-	/**
-	 * The scaling factor that is used to calculate the movable player's width and height.
-	 */
-	private static final double SCALING_FACTOR = 1.75;
-
-	private Button mMovablePlayerToggle;
-	private FrameLayout mButtonContainer;
-	private JWPlayerView mPlayerView;
-	private MovableFrameLayout mPlayerContainer;
-	private CoordinatorLayout mCoordinatorLayout;
-	private RelativeLayout mContentContainer;
-
-	private ViewGroup.LayoutParams mInitialLayoutParams;
-
-	private PlayerState mPlayerState;
+													VideoPlayerEvents.OnFullscreenListener {
+	private MovablePlayerView mPlayerView;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -55,234 +26,70 @@ public class MainActivity extends AppCompatActivity implements
 		setContentView(R.layout.activity_main);
 
 		// Get a reference to the CoordinatorLayout
-		mCoordinatorLayout = findViewById(R.id.coordinator_layout);
-		mContentContainer = findViewById(R.id.main_container);
-
-		// Get a reference to the FrameLayout that we will use to contain the player in.
-		mPlayerContainer = findViewById(R.id.player_container);
-		mButtonContainer = findViewById(R.id.button_container);
-		mMovablePlayerToggle = findViewById(R.id.movable_player_toggle);
-		mMovablePlayerToggle.setOnClickListener(new View.OnClickListener() {
-			@Override
-			public void onClick(View v) {
-				RelativeLayout.LayoutParams layoutParams = (RelativeLayout.LayoutParams)
-						mButtonContainer.getLayoutParams();
-				if (!mPlayerContainer.isMovable()) {
-					layoutParams.addRule(RelativeLayout.CENTER_IN_PARENT);
-					layoutParams.removeRule(RelativeLayout.ALIGN_PARENT_BOTTOM);
-				} else {
-					layoutParams.removeRule(RelativeLayout.CENTER_IN_PARENT);
-					// TODO: setup new layout params instead or removing rules
-					layoutParams.addRule(RelativeLayout.ALIGN_PARENT_BOTTOM);
-				}
-				mButtonContainer.setLayoutParams(layoutParams);
-				toggleMovablePlayer();
-			}
-		});
+		RelativeLayout mRelativeLayout = findViewById(R.id.relative_layout);
 
 		// Initialize a new JW Player.
-		mPlayerView = new JWPlayerView(this, new PlayerConfig.Builder()
+		mPlayerView = new MovablePlayerView(this, new PlayerConfig.Builder()
 				.file("https://content.jwplatform.com/manifests/mkZVAqxV.m3u8")
 				.build());
-
-		mPlayerView.addOnFullscreenListener(this);
-		mPlayerView.addOnPlayListener(this);
-		mPlayerView.addOnPauseListener(this);
-		mPlayerView.addOnCompleteListener(this);
-
+		DisplayMetrics displayMetrics = getResources().getDisplayMetrics();
+		RelativeLayout.LayoutParams params;
+		params = new RelativeLayout.LayoutParams(
+				ViewGroup.LayoutParams.MATCH_PARENT,
+				(displayMetrics.widthPixels / 16) * 9);
+		params.addRule(RelativeLayout.ALIGN_PARENT_TOP);
+		params.addRule(RelativeLayout.CENTER_HORIZONTAL);
+		mPlayerView.setLayoutParams(params);
 		// Add the View to the View Hierarchy.
-		mPlayerContainer.addView(mPlayerView);
-		setInitialLayoutParams();
-	}
+		mRelativeLayout.addView(mPlayerView);
+		mPlayerView.addOnFullscreenListener(this);
 
-	/**
-	 * Toggles the movable player:
-	 * <p>
-	 * If the player is not in a movable state:
-	 * - Disable player controls, since the skin does not scale nicely to smaller views
-	 * (you could create a custom skin for this purpose).
-	 * - Create a 'copy' of the current Layout Parameters of the PlayerContainer.
-	 * - Resize the player.
-	 * - Set an 'onTouchListener' on the container containing the player.
-	 * <p>
-	 * <p>
-	 * Else:
-	 * - Re-enable the player controls.
-	 * - Remove the 'onTouchListener'.
-	 * - Reset the Layout Parameters.
-	 */
-	private void toggleMovablePlayer() {
-		if (!mPlayerContainer.isMovable()) {
-			// Set the player container to movable, in order to intercept touch events.
-			mPlayerContainer.setMovable(true);
-
-			// Disable fullscreen rotation handling on the JW Player.
-			mPlayerView.setFullscreen(mPlayerView.getFullscreen(), false);
-
-			// Disable controls.
-			mPlayerView.setControls(false);
-			if (mPlayerState != PlayerState.PLAYING && mPlayerState != PlayerState.BUFFERING) {
-				// Start playback in case the user hasn't done this yet, since we don't want to have
-				// a movable player that does not play anything...
-				mPlayerView.play();
-			}
-
-			// Scale the player.
-			mInitialLayoutParams = mPlayerContainer.getLayoutParams();
-			int newWidth = (int)(mPlayerContainer.getWidth() / SCALING_FACTOR);
-			int newHeight = (int)(mPlayerContainer.getHeight() / SCALING_FACTOR);
-			RelativeLayout.LayoutParams layoutParams =
-					new RelativeLayout.LayoutParams(newWidth, newHeight);
-
-			// Position the player in the right bottom corner.
-			mPlayerContainer.setLayoutParams(getInitialMovablePlayerLayoutParams(layoutParams));
-
-			// Set an onTouchListener on the player which handles MotionEvents.
-			mPlayerContainer.setOnTouchListener(new View.OnTouchListener() {
-				@Override
-				public boolean onTouch(View v, MotionEvent event) {
-					if (v.getId() == R.id.player_container) {
-						RelativeLayout.LayoutParams layoutParams = (RelativeLayout.LayoutParams)v
-								.getLayoutParams();
-						switch (event.getAction()) {
-							case MotionEvent.ACTION_DOWN:
-								// Notify the MovablePlayerLayout that we started consuming
-								// events in order to receive ACTION_MOVE events.
-								return true;
-							case MotionEvent.ACTION_MOVE:
-								Log.d("GEORGE", "event.getRawY():" + event.getRawY());
-								Log.d("GEORGE", "v.getHeight():" + v.getHeight());
-								Log.d("GEORGE", "event.getRawX():" + event.getRawX());
-								Log.d("GEORGE", "v.getWidth():" + v.getWidth());
-
-								int topMargin = (int)event.getRawY() - v.getHeight();
-								int leftMargin = (int)event.getRawX() - v.getWidth();
-
-								// Make sure that the view can not go "out of bounds"
-								if (topMargin < 0) {
-									// Out of bounds: TOP
-									topMargin = 0;
-								}
-								if (topMargin > mContentContainer.getHeight() - mPlayerContainer
-										.getHeight()) {
-									// Out of bounds: BOTTOM
-									topMargin = mContentContainer.getHeight() - mPlayerContainer
-											.getHeight();
-								}
-
-								if (leftMargin < 0) {
-									// Out of bounds: LEFT
-									leftMargin = 0;
-								}
-								if (leftMargin > mContentContainer.getWidth() - mPlayerContainer
-										.getWidth()) {
-									// Out of bounds: RIGHT
-									leftMargin = mContentContainer.getWidth() - mPlayerContainer
-											.getWidth();
-								}
-
-								layoutParams.topMargin = topMargin;
-								layoutParams.leftMargin = leftMargin;
-
-								// Make sure the align rules have been removed.
-								layoutParams.removeRule(RelativeLayout.ALIGN_PARENT_BOTTOM);
-								layoutParams.removeRule(RelativeLayout.ALIGN_PARENT_RIGHT);
-								layoutParams.rightMargin = 0;
-								layoutParams.bottomMargin = 0;
-								// Set the new layout parameters
-								v.setLayoutParams(layoutParams);
-								return true;
-						}
-					}
-					return false;
-				}
-			});
-		} else {
-			// Disable the movable property of the MovableViewLayout.
-			mPlayerContainer.setMovable(false);
-			// Restore the initial layout parameters.
-			mPlayerContainer.setLayoutParams(mInitialLayoutParams);
-			// Remove the onTouchListener.
-			mPlayerContainer.setOnTouchListener(null);
-			// Re-enable the controls.
-			mPlayerView.setControls(true);
-			// Re-enable fullscreen rotation handling, and go to fullscreen if we're in landscape mode.
-			mPlayerView.setFullscreen(getResources().getConfiguration().orientation
-											  == Configuration.ORIENTATION_LANDSCAPE, true);
-		}
-	}
-
-	/**
-	 * Sets the initial layout parameters for the {@link JWPlayerView}.
-	 */
-	private void setInitialLayoutParams() {
-		DisplayMetrics displayMetrics = getResources().getDisplayMetrics();
-		if (getResources().getConfiguration().orientation == Configuration.ORIENTATION_PORTRAIT) {
-			mPlayerContainer.setLayoutParams(new RelativeLayout.LayoutParams(
-					ViewGroup.LayoutParams.MATCH_PARENT,
-					(displayMetrics.widthPixels / 16) * 9)); // 16:9
-		} else {
-			// We need to use height to calculate a 16:9 ratio since we're in landscape mode.
-			mPlayerContainer.setLayoutParams(new RelativeLayout.LayoutParams(
-					ViewGroup.LayoutParams.MATCH_PARENT,
-					(displayMetrics.heightPixels / 16) * 9)); // 16:9
-			// Toggle fullscreen, since we're in landscape mode.
-			mPlayerView.setFullscreen(true, true);
-		}
-	}
-
-	/**
-	 * Positions the movable player to the right bottom corner.
-	 *
-	 * @param layoutParams
-	 * @return
-	 */
-	private RelativeLayout.LayoutParams getInitialMovablePlayerLayoutParams(RelativeLayout.LayoutParams layoutParams) {
-		layoutParams.addRule(RelativeLayout.ALIGN_PARENT_BOTTOM);
-		layoutParams.addRule(RelativeLayout.ALIGN_PARENT_RIGHT);
-		DisplayMetrics displayMetrics = getResources().getDisplayMetrics();
-		layoutParams.setMargins(0,
-								0,
-								Math.round(displayMetrics.density * 16),
-								Math.round(displayMetrics.density * 16));
-		return layoutParams;
+		findViewById(R.id.movable_player_toggle).setOnClickListener(v -> {
+			mPlayerView.toggleDrag();
+		});
 	}
 
 
 	@Override
 	public void onConfigurationChanged(@NonNull Configuration newConfig) {
-		if (!mPlayerContainer.isMovable()) {
-			// Set fullscreen when the device is rotated to landscape, and not in movable player mode.
-			mPlayerView.setFullscreen(newConfig.orientation == Configuration.ORIENTATION_LANDSCAPE,
-									  true);
-		} else {
-			// When we rotate and the player is in movable mode, reset it's position.
-			mPlayerContainer.setLayoutParams(getInitialMovablePlayerLayoutParams(
-					(RelativeLayout.LayoutParams)mPlayerContainer.getLayoutParams()));
-		}
+		// Set fullscreen when the device is rotated to landscape, and not in movable player mode.
+		mPlayerView.setFullscreen(newConfig.orientation == Configuration.ORIENTATION_LANDSCAPE,
+								  true);
 		super.onConfigurationChanged(newConfig);
 	}
 
 
 	@Override
+	protected void onStop() {
+		super.onStop();
+		mPlayerView.onStop();
+	}
+
+	@Override
+	protected void onStart() {
+		super.onStart();
+		mPlayerView.onStart();
+	}
+
+	@Override
 	protected void onResume() {
 		// Let JW Player know that the app has returned from the background
-		mPlayerView.onResume();
 		super.onResume();
+		mPlayerView.onResume();
 	}
 
 	@Override
 	protected void onPause() {
 		// Let JW Player know that the app is going to the background
-		mPlayerView.onPause();
 		super.onPause();
+		mPlayerView.onPause();
 	}
 
 	@Override
 	protected void onDestroy() {
 		// Let JW Player know that the app is being destroyed
-		mPlayerView.onDestroy();
 		super.onDestroy();
+		mPlayerView.onDestroy();
 	}
 
 	@Override
@@ -298,45 +105,15 @@ public class MainActivity extends AppCompatActivity implements
 	}
 
 	@Override
-	public void onComplete(CompleteEvent completeEvent) {
-		mPlayerState = PlayerState.IDLE;
-	}
-
-	/**
-	 * Handles JW Player going to and returning from fullscreen by hiding the ActionBar
-	 *
-	 * @param fullscreenEvent has a getFullscreen which return s true if the player is fullscreen
-	 */
-	@Override
 	public void onFullscreen(FullscreenEvent fullscreenEvent) {
-		boolean fullscreen = fullscreenEvent.getFullscreen();
 		ActionBar actionBar = getSupportActionBar();
 		if (actionBar != null) {
-			if (fullscreen) {
-				/*
-				 * Because we are using a coordinator layout, which has setFitsSystemWindows set
-				 * to 'true' by default, we need to disable this when the JW Player View goes into
-				 * fullscreen. If we would not do this, the activity will not properly hide the
-				 * system UI.
-				 */
-				mCoordinatorLayout.setFitsSystemWindows(false);
+			if (fullscreenEvent.getFullscreen()) {
 				actionBar.hide();
-				mMovablePlayerToggle.setVisibility(View.GONE);
 			} else {
-				mCoordinatorLayout.setFitsSystemWindows(true);
 				actionBar.show();
-				mMovablePlayerToggle.setVisibility(View.VISIBLE);
 			}
 		}
 	}
 
-	@Override
-	public void onPause(PauseEvent pauseEvent) {
-		mPlayerState = pauseEvent.getOldState();
-	}
-
-	@Override
-	public void onPlay(PlayEvent playEvent) {
-		mPlayerState = playEvent.getOldState();
-	}
 }
