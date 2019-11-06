847671509460a9e9edd8b4b375422d1e5702fd15
gmsyrimis <gmsyrimis@gmail.com>
2019-11-06 13:30:09 -0500

    Chromecast demo init

---
+++ ChromecastDemo/README.md
@@ -0,0 +1,15 @@
+# JW Player SDK for Android Open Source Demo
+
+
+[![Join the chat at https://gitter.im/jwplayer/jwplayer-sdk-android-demo](https://badges.gitter.im/Join%20Chat.svg)](https://gitter.im/jwplayer/jwplayer-sdk-android-demo?utm_source=badge&utm_medium=badge&utm_campaign=pr-badge&utm_content=badge)
+
+This application contains an example implementation of the JW Player SDK for Android.
+
+#### Usage instructions:
+
+-	Clone the repository into your Android Studio workspace, `git clone git@github.com:jwplayer/jwplayer-sdk-android-demo.git`
+-	Open the AndroidManifest.xml file and replace {YOUR_LICENSE_KEY} with your license key
+
+The demo application should now build and run. For more information on how to use our SDK refer to our developer guide:
+
+[https://developer.jwplayer.com/sdk/android/docs/developer-guide/](https://developer.jwplayer.com/sdk/android/docs/developer-guide/)
---
+++ ChromecastDemo/app/src/main/java/com/jwplayer/chromecastdemo/JWPlayerFragmentExample.java
@@ -0,0 +1,87 @@
+package com.jwplayer.chromecastdemo;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
+import android.support.v7.app.AppCompatActivity;
+import android.view.KeyEvent;
+import android.view.Menu;
+import android.view.MenuItem;
+
+import com.google.android.gms.cast.framework.CastButtonFactory;
+import com.longtailvideo.jwplayer.JWPlayerSupportFragment;
+import com.longtailvideo.jwplayer.JWPlayerView;
+import com.longtailvideo.jwplayer.configuration.PlayerConfig;
+
+
+public class JWPlayerFragmentExample extends AppCompatActivity {
+
+
+	private JWPlayerSupportFragment mPlayerFragment;
+
+	private JWPlayerView mPlayerView;
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_jwplayerfragment);
+
+		// Construct a new JWPlayerSupportFragment (since we're using AppCompatActivity)
+		mPlayerFragment = JWPlayerSupportFragment.newInstance(new PlayerConfig.Builder()
+																	  .file("http://playertest.longtailvideo.com/adaptive/bipbop/gear4/prog_index.m3u8")
+																	  .build());
+
+		// Attach the Fragment to our layout
+		FragmentManager fm = getSupportFragmentManager();
+		FragmentTransaction ft = fm.beginTransaction();
+		ft.add(R.id.fragment_container, mPlayerFragment);
+		ft.commit();
+
+		// Make sure all the pending fragment transactions have been completed, otherwise
+		// mPlayerFragment.getPlayer() may return null
+		fm.executePendingTransactions();
+
+		// Get a reference to the JWPlayerView from the fragment
+		mPlayerView = mPlayerFragment.getPlayer();
+
+		// Keep the screen on during playback
+		new KeepScreenOnHandler(mPlayerView, getWindow());
+
+
+	}
+
+	@Override
+	public boolean onKeyDown(int keyCode, KeyEvent event) {
+		// Exit fullscreen when the user pressed the Back button
+		if (keyCode == KeyEvent.KEYCODE_BACK) {
+			if (mPlayerView.getFullscreen()) {
+				mPlayerView.setFullscreen(false, true);
+				return false;
+			}
+		}
+		return super.onKeyDown(keyCode, event);
+	}
+
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		getMenuInflater().inflate(R.menu.menu_jwplayerfragment, menu);
+		// Register the MediaRouterButton
+		CastButtonFactory.setUpMediaRouteButton(getApplicationContext(), menu,
+												R.id.media_route_menu_item);
+		return true;
+	}
+
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		switch (item.getItemId()) {
+			case R.id.switch_to_view:
+				Intent i = new Intent(this, JWPlayerViewExample.class);
+				startActivity(i);
+				return true;
+			default:
+				return super.onOptionsItemSelected(item);
+		}
+	}
+}
