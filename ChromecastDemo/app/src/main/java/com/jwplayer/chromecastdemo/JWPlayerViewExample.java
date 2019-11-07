385be0d83c6237a85287d2865c4ead6800f6d480
gmsyrimis <gmsyrimis@gmail.com>
2019-11-07 18:11:38 -0500

    Chromecast demo complete

--- ChromecastDemo/app/src/main/java/com/jwplayer/chromecastdemo/JWPlayerViewExample.java
+++ ChromecastDemo/app/src/main/java/com/jwplayer/chromecastdemo/JWPlayerViewExample.java
@@ -1,21 +1,27 @@
 package com.jwplayer.chromecastdemo;
 
-import android.content.Intent;
+import android.content.Context;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
 import android.content.res.Configuration;
 import android.os.Bundle;
-import android.support.v7.app.ActionBar;
-import android.support.v7.app.AppCompatActivity;
 import android.view.KeyEvent;
 import android.view.Menu;
-import android.view.MenuItem;
 
 import com.google.android.gms.cast.framework.CastButtonFactory;
 import com.google.android.gms.cast.framework.CastContext;
+import com.google.android.gms.cast.framework.CastState;
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.GoogleApiAvailability;
 import com.longtailvideo.jwplayer.JWPlayerView;
 import com.longtailvideo.jwplayer.events.FullscreenEvent;
 import com.longtailvideo.jwplayer.events.listeners.VideoPlayerEvents;
 import com.longtailvideo.jwplayer.media.playlists.PlaylistItem;
 
+import java.util.List;
+
+import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.AppCompatActivity;
 
 
 public class JWPlayerViewExample extends AppCompatActivity
@@ -25,14 +31,19 @@ public class JWPlayerViewExample extends AppCompatActivity
 
 	private CastContext mCastContext;
 
+	private static final String GooglePlayStorePackageNameOld = "com.google.market";
+	private static final String GooglePlayStorePackageNameNew = "com.android.vending";
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_jwplayerview);
 
-		mPlayerView = findViewById(R.id.jwplayer);
+		if (isGoogleApiAvailable(this)) {
+			mCastContext = CastContext.getSharedInstance(getApplicationContext());
+		}
 
+		mPlayerView = findViewById(R.id.jwplayer);
 
 		// Handle hiding/showing of ActionBar
 		mPlayerView.addOnFullscreenListener(this);
@@ -42,18 +53,34 @@ public class JWPlayerViewExample extends AppCompatActivity
 
 		// Load a media source
 		PlaylistItem pi = new PlaylistItem.Builder()
-				.file("https://playertest.longtailvideo.com/adaptive/bipbop/gear4/prog_index.m3u8")
-				.title("BipBop")
-				.description("A video player testing video.")
+				.file("https://content.jwplatform.com/manifests/1sc0kL2N.m3u8")
+				.title("Press Play")
+				.image("https://content.jwplatform.com/thumbs/1sc0kL2N.jpg")
+				.description("Press play with JW Player")
 				.build();
 
 		mPlayerView.load(pi);
 
-		// Get a reference to the CastContext
-		mCastContext = CastContext.getSharedInstance(this);
 
 	}
 
+	// Without the Google API's Chromecast won't work
+	private boolean isGoogleApiAvailable(Context context) {
+		boolean googlePlayStoreInstalled = false;
+		PackageManager packageManager = context.getApplicationContext().getPackageManager();
+		List<PackageInfo> packages = packageManager
+				.getInstalledPackages(PackageManager.GET_UNINSTALLED_PACKAGES);
+		for (PackageInfo packageInfo : packages) {
+			if (packageInfo.packageName.equals(GooglePlayStorePackageNameOld) ||
+					packageInfo.packageName.equals(GooglePlayStorePackageNameNew)) {
+				googlePlayStoreInstalled = true;
+				break;
+			}
+		}
+		boolean isGoogleApiAvailable = GoogleApiAvailability.getInstance()
+														  .isGooglePlayServicesAvailable(this) == ConnectionResult.SUCCESS;
+		return googlePlayStoreInstalled && isGoogleApiAvailable;
+	}
 
 	@Override
 	protected void onStart() {
@@ -109,7 +136,9 @@ public class JWPlayerViewExample extends AppCompatActivity
 	public void onFullscreen(FullscreenEvent fullscreenEvent) {
 		ActionBar actionBar = getSupportActionBar();
 		if (actionBar != null) {
-			if (fullscreenEvent.getFullscreen()) {
+			boolean isCasting = mCastContext != null ? mCastContext
+					.getCastState() == CastState.CONNECTED : false;
+			if (fullscreenEvent.getFullscreen() && !isCasting) {
 				actionBar.hide();
 			} else {
 				actionBar.show();
@@ -121,23 +150,10 @@ public class JWPlayerViewExample extends AppCompatActivity
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 		getMenuInflater().inflate(R.menu.menu_jwplayerview, menu);
-
 		// Register the MediaRouterButton
 		CastButtonFactory.setUpMediaRouteButton(getApplicationContext(), menu,
 												R.id.media_route_menu_item);
-
 		return true;
 	}
 
-	@Override
-	public boolean onOptionsItemSelected(MenuItem item) {
-		switch (item.getItemId()) {
-			case R.id.switch_to_fragment:
-				Intent i = new Intent(this, JWPlayerFragmentExample.class);
-				startActivity(i);
-				return true;
-			default:
-				return super.onOptionsItemSelected(item);
-		}
-	}
 }
