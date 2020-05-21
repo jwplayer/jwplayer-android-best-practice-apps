b8236429f060b1a8d99778ec8fc70750cb48d24a
Gilberto Hernandez G <ghernandez.9002@gmail.com>
2020-05-21 16:03:54 -0500

    feat: remove unused files and code implementation

--- FullscreenWithoutRotationDemo/app/src/main/java/com/jwplayer/opensourcedemo/JWPlayerViewExample.java
+++ FullscreenWithoutRotationDemo/app/src/main/java/com/jwplayer/opensourcedemo/JWPlayerViewExample.java
@@ -1,31 +1,22 @@
 package com.jwplayer.opensourcedemo;
 
-import android.content.Intent;
 import android.content.res.Configuration;
 import android.os.Bundle;
-import android.view.KeyEvent;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.view.ViewGroup;
 
 import com.longtailvideo.jwplayer.JWPlayerView;
-import com.longtailvideo.jwplayer.events.FullscreenEvent;
-import com.longtailvideo.jwplayer.events.listeners.VideoPlayerEvents;
 import com.longtailvideo.jwplayer.fullscreen.FullscreenHandler;
 import com.longtailvideo.jwplayer.media.playlists.PlaylistItem;
 
-import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
 import java.lang.reflect.Constructor;
 
 
-public class JWPlayerViewExample extends AppCompatActivity
-		implements VideoPlayerEvents.OnFullscreenListener {
+public class JWPlayerViewExample extends AppCompatActivity {
 
 	private JWPlayerView mPlayerView;
 
-	private CallbackScreen mCallbackScreen;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -34,18 +25,8 @@ public class JWPlayerViewExample extends AppCompatActivity
 
 		mPlayerView = findViewById(R.id.jwplayer);
 
-
-		// Handle hiding/showing of ActionBar
-		mPlayerView.addOnFullscreenListener(this);
-
-		// Keep the screen on during playback
-		new KeepScreenOnHandler(mPlayerView, getWindow());
-
+		// set screen handler for manage the rotation of the screen
 		mPlayerView.setFullscreenHandler(new FullScreenHandler_NoRotation(mPlayerView));
-
-		// Event Logging
-		mCallbackScreen = findViewById(R.id.callback_screen);
-		mCallbackScreen.registerListeners(mPlayerView);
 
 		// Load a media source
 		PlaylistItem pi = new PlaylistItem.Builder()
@@ -86,58 +67,6 @@ public class JWPlayerViewExample extends AppCompatActivity
 	protected void onDestroy() {
 		super.onDestroy();
 		mPlayerView.onDestroy();
-	}
-
-	@Override
-	public void onConfigurationChanged(Configuration newConfig) {
-		// Set fullscreen when the device is rotated to landscape
-		mPlayerView.setFullscreen(newConfig.orientation == Configuration.ORIENTATION_LANDSCAPE,
-								  true);
-		super.onConfigurationChanged(newConfig);
-	}
-
-	@Override
-	public boolean onKeyDown(int keyCode, KeyEvent event) {
-		// Exit fullscreen when the user pressed the Back button
-		if (keyCode == KeyEvent.KEYCODE_BACK) {
-			if (mPlayerView.getFullscreen()) {
-				mPlayerView.setFullscreen(false, true);
-				return false;
-			}
-		}
-		return super.onKeyDown(keyCode, event);
-	}
-
-	@Override
-	public void onFullscreen(FullscreenEvent fullscreenEvent) {
-		ActionBar actionBar = getSupportActionBar();
-		if (actionBar != null) {
-			if (fullscreenEvent.getFullscreen()) {
-				actionBar.hide();
-			} else {
-				actionBar.show();
-			}
-		}
-	}
-
-
-	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-		getMenuInflater().inflate(R.menu.menu_jwplayerview, menu);
-
-		return true;
-	}
-
-	@Override
-	public boolean onOptionsItemSelected(MenuItem item) {
-		switch (item.getItemId()) {
-			case R.id.switch_to_fragment:
-				Intent i = new Intent(this, JWPlayerFragmentExample.class);
-				startActivity(i);
-				return true;
-			default:
-				return super.onOptionsItemSelected(item);
-		}
 	}
 
 	public class FullScreenHandler_NoRotation implements FullscreenHandler {
