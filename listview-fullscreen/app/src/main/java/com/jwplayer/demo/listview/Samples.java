c4ae2fd442d701ee222531b26a7d57351e299c98
Gilberto Hernandez G <ghernandez.9002@gmail.com>
2021-11-19 09:40:37 -0600

    fix: network security config

--- listview-fullscreen/app/src/main/java/com/jwplayer/demo/listview/Samples.java
+++ listview-fullscreen/app/src/main/java/com/jwplayer/demo/listview/Samples.java
@@ -18,13 +18,13 @@ public class Samples {
             new PlaylistItem.Builder()
                     .title("BipBop")
                     .description("4x3 Apple HLS")
-                    .file("https://devimages.apple.com.edgekey.net/streaming/examples/bipbop_4x3/"
+                    .file("http://devimages.apple.com.edgekey.net/streaming/examples/bipbop_4x3/"
                             + "bipbop_4x3_variant.m3u8")
                     .build(),
             new PlaylistItem.Builder()
                     .title("BipBop")
                     .description("16x9 Apple HLS")
-                    .file("https://devimages.apple.com.edgekey.net/streaming/examples/bipbop_16x9/"
+                    .file("http://devimages.apple.com.edgekey.net/streaming/examples/bipbop_16x9/"
                             + "bipbop_16x9_variant.m3u8")
                     .build(),
             new PlaylistItem.Builder()
@@ -45,13 +45,13 @@ public class Samples {
             new PlaylistItem.Builder()
                     .title("BipBop")
                     .description("4x3 Apple HLS")
-                    .file("https://devimages.apple.com.edgekey.net/streaming/examples/bipbop_4x3/"
+                    .file("http://devimages.apple.com.edgekey.net/streaming/examples/bipbop_4x3/"
                             + "bipbop_4x3_variant.m3u8")
                     .build(),
             new PlaylistItem.Builder()
                     .title("BipBop")
                     .description("16x9 Apple HLS")
-                    .file("https://devimages.apple.com.edgekey.net/streaming/examples/bipbop_16x9/"
+                    .file("http://devimages.apple.com.edgekey.net/streaming/examples/bipbop_16x9/"
                             + "bipbop_16x9_variant.m3u8")
                     .build(),
             new PlaylistItem.Builder()
