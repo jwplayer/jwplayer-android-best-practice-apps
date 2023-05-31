bbe62925bf3f55fb92e261d3d13119563a33bb0b
George Syrimis <gmsyrimis@gmail.com>
2023-05-31 16:22:41 -0400

    [DEMO] Offline Delegate (#700)

---
+++ OfflineDelegate/app/src/main/java/com/jwplayer/offlinedelegate/MainActivity.java
@@ -0,0 +1,190 @@
+package com.jwplayer.offlinedelegate;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.android.exoplayer2.RenderersFactory;
+import com.google.android.exoplayer2.offline.Download;
+import com.google.android.exoplayer2.offline.DownloadService;
+import com.jwplayer.offlinedelegate.exo.DemoDownloadService;
+import com.jwplayer.offlinedelegate.exo.DemoUtil;
+import com.jwplayer.offlinedelegate.exo.DownloadTracker;
+import com.jwplayer.offlinedelegate.exo.OfflineDelegateImpl;
+import com.jwplayer.pub.api.ExoConverter;
+import com.jwplayer.pub.api.JWPlayer;
+import com.jwplayer.pub.api.configuration.PlayerConfig;
+import com.jwplayer.pub.api.license.LicenseUtil;
+import com.jwplayer.pub.api.media.captions.Caption;
+import com.jwplayer.pub.api.media.captions.CaptionType;
+import com.jwplayer.pub.api.media.playlists.PlaylistItem;
+import com.jwplayer.pub.view.JWPlayerView;
+
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity
+        implements NetworkTracker.NetworkStatusChangedListener {
+
+    private NetworkTracker mNetworkTracker;
+
+    private TextView mNetworkStatus;
+    private TextView mContentStatus;
+    private JWPlayerView mPlayerView;
+    private JWPlayer mPlayer;
+
+    private final PlaylistItem mOnlineSideloaded = new PlaylistItem.Builder()
+            .mediaId("QUxmKbSS")
+            .file("https://cdn.jwplayer.com/manifests/QUxmKbSS.m3u8")
+            .title("Online Sideloaded")
+            .tracks(new ArrayList<Caption>() {{
+                add(new Caption.Builder()
+                            .file("https://content.jwplatform.com/tracks/a2J6cY6H.srt")
+                            .kind(CaptionType.CAPTIONS)
+                            .label("English")
+                            .build());
+            }})
+            .build();
+    private final PlaylistItem mOfflineSideloaded = new PlaylistItem.Builder()
+            .mediaId("3XnJSIm4")
+            .title("Offline Sideloaded")
+            .file("https://cdn.jwplayer.com/manifests/3XnJSIm4.m3u8")
+            .tracks(new ArrayList<Caption>() {{
+                add(new Caption.Builder()
+                            .file("asset:///Sintel_en.srt")
+                            .kind(CaptionType.CAPTIONS)
+                            .label("English")
+                            .build());
+                add(new Caption.Builder()
+                            .file("asset:///Sintel_es.srt")
+                            .kind(CaptionType.CAPTIONS)
+                            .label("Spanish")
+                            .build());
+                add(new Caption.Builder()
+                            .file("asset:///Sintel_jp.srt")
+                            .kind(CaptionType.CAPTIONS)
+                            .label("Japanese")
+                            .build());
+            }})
+            .build();
+    private final PlaylistItem mInManifest = new PlaylistItem.Builder()
+            .mediaId("70zwzhEM")
+            .title("In Manifest")
+            .file("https://devstreaming-cdn.apple.com/videos/streaming/examples/bipbop_16x9/bipbop_16x9_variant.m3u8")
+            .build();
+
+    private PlaylistItem mCurrentItem = mOnlineSideloaded;
+
+    private OfflineDelegateImpl mOfflineDelegate;
+
+    // ExoPlayer demo code
+    private DownloadTracker downloadTracker;
+    private RenderersFactory mRenderersFactory;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        mNetworkStatus = findViewById(R.id.network_status);
+        mContentStatus = findViewById(R.id.content_status);
+        mPlayerView = findViewById(R.id.player);
+
+        new LicenseUtil().setLicenseKey(this, YOUR_LICENSE_KEY);
+        mNetworkTracker = new NetworkTracker(getApplicationContext(), this);
+
+        mRenderersFactory = DemoUtil.buildRenderersFactory(this, false);
+        downloadTracker = DemoUtil.getDownloadTracker(this);
+        mOfflineDelegate = new OfflineDelegateImpl(getApplicationContext());
+
+        updateContentStatus();
+
+        try {
+            DownloadService.start(this, DemoDownloadService.class);
+        } catch (IllegalStateException e) {
+            DownloadService.startForeground(this, DemoDownloadService.class);
+        }
+
+        findViewById(R.id.online_sideloaded).setOnClickListener(v -> {
+            mCurrentItem = mOnlineSideloaded;
+            updateContentStatus();
+        });
+        findViewById(R.id.offline_sideloaded).setOnClickListener(v -> {
+            mCurrentItem = mOfflineSideloaded;
+            updateContentStatus();
+        });
+        findViewById(R.id.in_manifest).setOnClickListener(v -> {
+            mCurrentItem = mInManifest;
+            updateContentStatus();
+        });
+        findViewById(R.id.prepare_download).setOnClickListener(v -> {
+            if (!mNetworkTracker.isOnline()) {
+                Toast.makeText(this, "Offline, cannot download content", Toast.LENGTH_SHORT).show();
+                return;
+            }
+            boolean success = downloadTracker.addDownload(
+                    getSupportFragmentManager(),
+                    ExoConverter.toMediaItem(mCurrentItem),
+                    mRenderersFactory
+            );
+            if (!success) {
+                Toast.makeText(this,
+                               "Content already downloaded or in progress",
+                               Toast.LENGTH_SHORT
+                ).show();
+            }
+        });
+        findViewById(R.id.remove_download).setOnClickListener(v -> {
+            boolean success = downloadTracker.removeDownload(mCurrentItem.getMediaId());
+            Toast.makeText(this,
+                           success ? "Content download removed" : "No content to be removed",
+                           Toast.LENGTH_SHORT
+            ).show();
+        });
+        findViewById(R.id.setup).setOnClickListener(v -> {
+            Download download = mOfflineDelegate.getDownload(mCurrentItem.getMediaId());
+            boolean isDownloaded = download != null && download.state == Download.STATE_COMPLETED;
+            if (mNetworkTracker.isOnline() || isDownloaded) {
+                String message = mNetworkTracker.isOnline() ? "Online" : "Offline";
+                message += ", ";
+                message += isDownloaded ? "Using downloaded content" : "Not Downloaded";
+                Toast.makeText(this, message, Toast.LENGTH_SHORT).show();
+                mPlayerView.setVisibility(View.VISIBLE);
+
+                // Pass in your OfflineDelegate Implementation
+                mPlayer = mPlayerView.getPlayer(this, mOfflineDelegate);
+
+                mPlayer.setup(new PlayerConfig.Builder()
+                                      .playlist(new ArrayList<PlaylistItem>() {{
+                                          add(mCurrentItem);
+                                      }})
+                                      .build()
+                );
+            } else {
+                mPlayerView.setVisibility(View.GONE);
+                Toast.makeText(this, "Offline, no downloaded content", Toast.LENGTH_SHORT)
+                     .show();
+            }
+        });
+    }
+
+    private void updateContentStatus() {
+        Download download = mOfflineDelegate.getDownload(mCurrentItem.getMediaId());
+        boolean isDownloaded = download != null && download.state == Download.STATE_COMPLETED;
+        mContentStatus.setText(mCurrentItem.getTitle() + ": " + (isDownloaded ? "Downloaded" : "Not Downloaded"));
+    }
+
+
+    @Override
+    public void onNetworkStatusChanged(boolean isOnline) {
+        mNetworkStatus.setText("Network Status: " + (isOnline ? "Online" : "Offline"));
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        mNetworkTracker.destroy();
+    }
+}
---
+++ OfflineDelegate/app/src/main/java/com/jwplayer/offlinedelegate/exo/DemoUtil.java
@@ -0,0 +1,179 @@
+/*
+ * Copyright (C) 2016 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.jwplayer.offlinedelegate.exo;
+
+import android.content.Context;
+
+import com.google.android.exoplayer2.DefaultRenderersFactory;
+import com.google.android.exoplayer2.RenderersFactory;
+import com.google.android.exoplayer2.database.DatabaseProvider;
+import com.google.android.exoplayer2.database.StandaloneDatabaseProvider;
+import com.google.android.exoplayer2.offline.DownloadManager;
+import com.google.android.exoplayer2.ui.DownloadNotificationHelper;
+import com.google.android.exoplayer2.upstream.DataSource;
+import com.google.android.exoplayer2.upstream.DefaultDataSource;
+import com.google.android.exoplayer2.upstream.DefaultHttpDataSource;
+import com.google.android.exoplayer2.upstream.cache.Cache;
+import com.google.android.exoplayer2.upstream.cache.CacheDataSource;
+import com.google.android.exoplayer2.upstream.cache.NoOpCacheEvictor;
+import com.google.android.exoplayer2.upstream.cache.SimpleCache;
+
+import java.io.File;
+import java.net.CookieHandler;
+import java.net.CookieManager;
+import java.net.CookiePolicy;
+import java.util.concurrent.Executors;
+
+/** Utility methods for the demo app. */
+public final class DemoUtil {
+
+  public static final String DOWNLOAD_NOTIFICATION_CHANNEL_ID = "download_channel";
+
+  /**
+   * Whether the demo application uses Cronet for networking. Note that Cronet does not provide
+   * automatic support for cookies (https://github.com/google/ExoPlayer/issues/5975).
+   *
+   * <p>If set to false, the platform's default network stack is used with a {@link CookieManager}
+   * configured in {@link #getHttpDataSourceFactory}.
+   */
+  private static final boolean USE_CRONET_FOR_NETWORKING = true;
+
+  private static final String TAG = "DemoUtil";
+  private static final String DOWNLOAD_CONTENT_DIRECTORY = "downloads";
+
+  private static DataSource.Factory dataSourceFactory;
+  private static DataSource.Factory httpDataSourceFactory;
+  private static DatabaseProvider databaseProvider;
+  private static File downloadDirectory;
+  private static Cache downloadCache;
+  private static DownloadManager downloadManager;
+  private static DownloadTracker downloadTracker;
+  private static DownloadNotificationHelper downloadNotificationHelper;
+
+  /** Returns whether extension renderers should be used. */
+  public static boolean useExtensionRenderers() {
+    return false;
+  }
+
+  public static RenderersFactory buildRenderersFactory(
+      Context context, boolean preferExtensionRenderer) {
+    @DefaultRenderersFactory.ExtensionRendererMode
+    int extensionRendererMode =
+        useExtensionRenderers()
+            ? (preferExtensionRenderer
+                ? DefaultRenderersFactory.EXTENSION_RENDERER_MODE_PREFER
+                : DefaultRenderersFactory.EXTENSION_RENDERER_MODE_ON)
+            : DefaultRenderersFactory.EXTENSION_RENDERER_MODE_OFF;
+    return new DefaultRenderersFactory(context.getApplicationContext())
+        .setExtensionRendererMode(extensionRendererMode);
+  }
+
+  public static synchronized DataSource.Factory getHttpDataSourceFactory(Context context) {
+    if (httpDataSourceFactory == null) {
+      if (httpDataSourceFactory == null) {
+        // We don't want to use Cronet, or we failed to instantiate a CronetEngine.
+        CookieManager cookieManager = new CookieManager();
+        cookieManager.setCookiePolicy(CookiePolicy.ACCEPT_ORIGINAL_SERVER);
+        CookieHandler.setDefault(cookieManager);
+        httpDataSourceFactory = new DefaultHttpDataSource.Factory();
+      }
+    }
+    return httpDataSourceFactory;
+  }
+
+  /** Returns a {@link DataSource.Factory}. */
+  public static synchronized DataSource.Factory getDataSourceFactory(Context context) {
+    if (dataSourceFactory == null) {
+      context = context.getApplicationContext();
+      DefaultDataSource.Factory upstreamFactory =
+          new DefaultDataSource.Factory(context, getHttpDataSourceFactory(context));
+      dataSourceFactory = buildReadOnlyCacheDataSource(upstreamFactory, getDownloadCache(context));
+    }
+    return dataSourceFactory;
+  }
+
+  public static synchronized DownloadNotificationHelper getDownloadNotificationHelper(
+      Context context) {
+    if (downloadNotificationHelper == null) {
+      downloadNotificationHelper =
+          new DownloadNotificationHelper(context, DOWNLOAD_NOTIFICATION_CHANNEL_ID);
+    }
+    return downloadNotificationHelper;
+  }
+
+  public static synchronized DownloadManager getDownloadManager(Context context) {
+    ensureDownloadManagerInitialized(context);
+    return downloadManager;
+  }
+
+  public static synchronized DownloadTracker getDownloadTracker(Context context) {
+    ensureDownloadManagerInitialized(context);
+    return downloadTracker;
+  }
+
+  private static synchronized Cache getDownloadCache(Context context) {
+    if (downloadCache == null) {
+      File downloadContentDirectory =
+          new File(getDownloadDirectory(context), DOWNLOAD_CONTENT_DIRECTORY);
+      downloadCache =
+          new SimpleCache(
+              downloadContentDirectory, new NoOpCacheEvictor(), getDatabaseProvider(context));
+    }
+    return downloadCache;
+  }
+
+  private static synchronized void ensureDownloadManagerInitialized(Context context) {
+    if (downloadManager == null) {
+      downloadManager =
+          new DownloadManager(
+              context,
+              getDatabaseProvider(context),
+              getDownloadCache(context),
+              getHttpDataSourceFactory(context),
+              Executors.newFixedThreadPool(/* nThreads= */ 6));
+      downloadTracker =
+          new DownloadTracker(context, getHttpDataSourceFactory(context), downloadManager);
+    }
+  }
+
+  private static synchronized DatabaseProvider getDatabaseProvider(Context context) {
+    if (databaseProvider == null) {
+      databaseProvider = new StandaloneDatabaseProvider(context);
+    }
+    return databaseProvider;
+  }
+
+  private static synchronized File getDownloadDirectory(Context context) {
+    if (downloadDirectory == null) {
+      downloadDirectory = context.getExternalFilesDir(/* type= */ null);
+      if (downloadDirectory == null) {
+        downloadDirectory = context.getFilesDir();
+      }
+    }
+    return downloadDirectory;
+  }
+
+  private static CacheDataSource.Factory buildReadOnlyCacheDataSource(
+      DataSource.Factory upstreamFactory, Cache cache) {
+    return new CacheDataSource.Factory()
+        .setCache(cache)
+        .setUpstreamDataSourceFactory(upstreamFactory)
+        .setCacheWriteDataSinkFactory(null)
+        .setFlags(CacheDataSource.FLAG_IGNORE_CACHE_ON_ERROR);
+  }
+
+  private DemoUtil() {}
+}
