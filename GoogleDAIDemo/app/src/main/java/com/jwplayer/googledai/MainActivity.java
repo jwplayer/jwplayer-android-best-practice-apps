77f014c75119877dbe3107111c77fa3a5eee778c
Joe Milham <jmilham21@gmail.com>
2024-01-10 11:58:47 -0800

    upgrade GoogleDAIDemo: Gradle8, JW SDK version / AndroidX Media3.

--- GoogleDAIDemo/app/src/main/java/com/jwplayer/googledai/MainActivity.java
+++ GoogleDAIDemo/app/src/main/java/com/jwplayer/googledai/MainActivity.java
@@ -31,10 +31,10 @@ public class MainActivity extends AppCompatActivity
         // INFO: Overwrite BuildConfig.JWPLAYER_LICENSE_KEY with your license here
 		// [OR] change in app-level build.gradle
 		// [OR] set JWPLAYER_LICENSE_KEY as environment variable
-		LicenseUtil.setLicenseKey(this, BuildConfig.JWPLAYER_LICENSE_KEY);
+		new LicenseUtil().setLicenseKey(this, BuildConfig.JWPLAYER_LICENSE_KEY);
 
         mPlayerView = findViewById(R.id.jwplayer);
-        mPlayer = mPlayerView.getPlayer();
+        mPlayer = mPlayerView.getPlayer(this);
 
         List<PlaylistItem> playlist = new ArrayList<>();
         //videoId: Identifier of the DAI video to be displayed, used for video on demand
