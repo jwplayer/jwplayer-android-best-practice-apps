c91457b18a5394fc4952cebe3277b32cfd9ff3f8
Gilberto Hernandez G <ghernandez.9002@gmail.com>
2024-01-25 11:31:57 -0600

    task: initial duplicate project on java

---
+++ Kotlin CustomUi/README.md
@@ -0,0 +1,9 @@
+# Custom UI Demo
+
+This application contains an example implementation of creating a Custom UI with Kotlin DSL using the JW Player SDK for Android.
+
+#### Usage instructions:
+
+-	Open the `JWPlayerViewExample` Activity and replace `YOUR_LICENSE_KEY` with your license key
+
+The demo application should now build and run. For more information on how to use our SDK refer to our [developer guide](https://developer.jwplayer.com/jwplayer/docs/android-get-started)
---
+++ Kotlin CustomUi/app/src/main/java/com/jwplayer/customui/JWPlayerViewExample.java
@@ -0,0 +1,112 @@
+package com.jwplayer.customui;
+
+import android.content.res.Configuration;
+import android.os.Bundle;
+import android.view.KeyEvent;
+import android.view.ViewGroup;
+import android.webkit.WebView;
+import android.widget.FrameLayout;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.view.ContextThemeWrapper;
+
+import com.jwplayer.pub.api.JWPlayer;
+import com.jwplayer.pub.api.UiGroup;
+import com.jwplayer.pub.api.configuration.PlayerConfig;
+import com.jwplayer.pub.api.configuration.UiConfig;
+import com.jwplayer.pub.api.events.EventType;
+import com.jwplayer.pub.api.events.FullscreenEvent;
+import com.jwplayer.pub.api.events.listeners.VideoPlayerEvents;
+import com.jwplayer.pub.api.license.LicenseUtil;
+import com.jwplayer.pub.view.JWPlayerView;
+
+
+public class JWPlayerViewExample extends AppCompatActivity
+        implements VideoPlayerEvents.OnFullscreenListener {
+
+    private JWPlayerView mPlayerView;
+
+    private JWPlayer mPlayer;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_jwplayerview);
+        WebView.setWebContentsDebuggingEnabled(true);
+        // TODO: Add your license key
+        new LicenseUtil().setLicenseKey(this, "izT2FVT/QOIkATeUUP0kt9qOJFZsEl6hlpl47A/zwet2Q/zm");
+        mPlayerView = findViewById(R.id.jwplayer);
+        mPlayerView.getPlayerAsync(this, this, jwPlayer -> {
+            mPlayer = jwPlayer;
+            setupPlayer();
+        });
+
+    }
+
+    private void setupPlayer() {
+        // Handle hiding/showing of ActionBar
+        mPlayer.addListener(EventType.FULLSCREEN, JWPlayerViewExample.this);
+
+        // Keep the screen on during playback
+        new KeepScreenOnHandler(mPlayer, getWindow());
+
+        // Load a media source
+        PlayerConfig config = new PlayerConfig.Builder()
+                .playlistUrl("https://cdn.jwplayer.com/v2/playlists/3jBCQ2MI?format=json")
+                .uiConfig(new UiConfig.Builder()
+                        .displayAllControls()
+                        .hide(UiGroup.NEXT_UP)
+                        .build()
+                )
+                .build();
+        // Call setup before binding the ViewModels because setup updates the ViewModels
+        mPlayer.setup(config);
+
+        // We create a MyControls ViewGroup in which we can control the positioning of the Views
+        MyControls controls = new MyControls(new ContextThemeWrapper(this, R.style.AppTheme));
+        FrameLayout.LayoutParams params = new FrameLayout.LayoutParams(
+                ViewGroup.LayoutParams.MATCH_PARENT,
+                ViewGroup.LayoutParams.MATCH_PARENT
+        );
+        controls.setLayoutParams(params);
+        mPlayerView.addView(controls);
+        controls.bind(mPlayer, this);
+    }
+
+
+    @Override
+    public void onConfigurationChanged(@NonNull Configuration newConfig) {
+        super.onConfigurationChanged(newConfig);
+        if (!mPlayer.isInPictureInPictureMode()) {
+            final boolean isFullscreen = newConfig.orientation == Configuration.ORIENTATION_LANDSCAPE;
+            mPlayer.setFullscreen(isFullscreen, true);
+        }
+    }
+
+    @Override
+    public boolean onKeyDown(int keyCode, KeyEvent event) {
+        // Exit fullscreen when the user pressed the Back button
+        if (keyCode == KeyEvent.KEYCODE_BACK) {
+            if (mPlayer.getFullscreen()) {
+                mPlayer.setFullscreen(false, true);
+                return false;
+            }
+        }
+        return super.onKeyDown(keyCode, event);
+    }
+
+
+    @Override
+    public void onFullscreen(FullscreenEvent fullscreenEvent) {
+        ActionBar actionBar = getSupportActionBar();
+        if (actionBar != null) {
+            if (fullscreenEvent.getFullscreen()) {
+                actionBar.hide();
+            } else {
+                actionBar.show();
+            }
+        }
+    }
+}
